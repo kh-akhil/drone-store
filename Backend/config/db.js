f489b6418153aba8816c9fc68ea53fc43ffa9f66
K H Akhil <khakhil.khakhil@gmail.com>
2024-08-02 16:01:05 +0530

    improved security

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://khakhil:akhil007@cluster0.nuwebsa.mongodb.net/drone-store').then(()=>console.log("DB Connected"));
+    await mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.nuwebsa.mongodb.net/drone-store`).then(()=>console.log("DB Connected"));
 }
